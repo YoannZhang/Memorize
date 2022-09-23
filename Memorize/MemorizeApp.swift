499e5e66c0e302796b1f885f63b189f33a696578
Yoann Z <yoannzhang009@gmail.com>
2022-09-23 14:35:09 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Yoann Zhang on 2022/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
