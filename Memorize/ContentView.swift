96ae62a5310a0ac4c5db02881656fadac66ee6e1
Yoann Z <yoannzhang009@gmail.com>
2022-09-28 20:55:01 +0800

    my first commit

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,18 +9,71 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(.red)
+
+//        return RoundedRectangle(cornerRadius: 20.0)
+//            .stroke(lineWidth: 3)
+//            .padding(.horizontal)
+//            .foregroundColor(.red)
+//        Text("Hello, CS193p!")
+//            .foregroundColor(.orange)
+//            .padding(.all)
     }
 }
+
+struct CardView: View{
+    @State var isFaceUp: Bool = true
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20.0)
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .stroke(lineWidth: 3)
+                Text("✈️")
+                    .font(.largeTitle)
+            } else {
+                shape
+                    .fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-@main
+@main //
 struct MemorizeApp: App {
     var body: some Scene {
         WindowGroup {
